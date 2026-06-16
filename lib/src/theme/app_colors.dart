4a0c6ab212acb5aaba71079c03ce94c7f8e9199b
Parcibal12 <danielirigoyen984@gmail.com>
2026-06-16 00:47:33 -0400

    feature: Add AppColors and clean up default tests

---
+++ lib/src/theme/app_colors.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primaryGreen = Color(0xFF14B8A6);
+  static const Color primaryDark = Color(0xFF0D9488);
+  static const Color primaryLight = Color(0xFF2DD4BF);
+  
+  static const Color textHeader = Color(0xFF1E293B);
+  static const Color textSubtitle = Color(0xFF94A3B8);
+  static const Color textNumber = Color(0xFF334155);
+
+  static const Color backgroundWhite = Colors.white;
+  static const Color surfaceKeyboard = Color(0xFFF8FAFC);
+  static const Color borderLight = Color(0xFFF1F5F9);
+  static const Color indicatorInactive = Color(0xFFE2E8F0);
+
+  static const Color anxietyOrange = Color(0xFFF97316);
+  static const Color angerPurple = Color(0xFFA855F7);
+  static const Color sadnessBlue = Color(0xFF3B82F6);
+  static const Color calmTeal = primaryGreen;
+  static const Color neutralGray = textSubtitle;
+}
